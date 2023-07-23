e65713f003225ba2ee01084086b7d625a3e773c7
WebDeverDan <danielbvideo@gmail.com>
2023-07-23 12:33:49 -0400

    Updates

---
+++ precache-manifest.141c767f42a23481d6d7fd5ee675c02d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "70fd250e2ebeee09da8e",
+    "url": "/ShouldISkateToday/static/css/main.3411c314.chunk.css"
+  },
+  {
+    "revision": "70fd250e2ebeee09da8e",
+    "url": "/ShouldISkateToday/static/js/main.faf00fae.chunk.js"
+  },
+  {
+    "revision": "c4e8484d75222e3510eb",
+    "url": "/ShouldISkateToday/static/js/runtime~main.d9ed48cd.js"
+  },
+  {
+    "revision": "b2a7b2ffc41f757f85a1",
+    "url": "/ShouldISkateToday/static/css/2.70401eca.chunk.css"
+  },
+  {
+    "revision": "b2a7b2ffc41f757f85a1",
+    "url": "/ShouldISkateToday/static/js/2.7ad01c60.chunk.js"
+  },
+  {
+    "revision": "34436f37db1b1443ae93cffa2ebe7800",
+    "url": "/ShouldISkateToday/static/media/photo-1615066037299-7fd7fe32686b.34436f37.avif"
+  },
+  {
+    "revision": "3f2fca8718315615bdedb4aa93c45f9d",
+    "url": "/ShouldISkateToday/index.html"
+  }
+];
